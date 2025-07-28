69f969f3841943a35f733e57307e36a09e782eb7
Yokeshwaran Shankar <yokeshwaran.shankar@gmail.com>
2025-07-28 19:22:27 +0530

    Data Transformation Done

--- src/exception.py
+++ src/exception.py
@@ -16,7 +16,7 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message = error_message_details(error_message,error_detail=error_detail)
         
-    def __str___(self):
+    def __str__(self):
         return self.error_message
         
     
